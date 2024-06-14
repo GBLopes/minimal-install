2d45338cc28a475a930bf63a40510e97dd6a437d
GBLopes <guilherme.bl90@gmail.com>
2024-06-14 10:29:54 -0300

    Pequeno ajuste no config_xfce

--- config_xfce.sh
+++ config_xfce.sh
@@ -7,10 +7,10 @@ if [ "$(id -u)" == "0" ]; then
 fi && \
 
 
-mkdir -p /home/$USER/.config/plank/dock1/launchers/ && \
+mkdir -p "/home/$USER/.config/plank/dock1/launchers/" && \
 
-cp -r utils/plank/*.dockitem /home/$USER/.config/plank/dock1/launchers && \
+cp -R -f utils/plank/*.dockitem "/home/$USER/.config/plank/dock1/launchers" && \
 
-cp -r -f utils/configs/xfce4/* /home/$USER/.config/xfce4 && \
+cp -R -f utils/configs/xfce4/* "/home/$USER/.config/xfce4" && \
 
 echo 'Configuração do ambiente xfce concluída!'
