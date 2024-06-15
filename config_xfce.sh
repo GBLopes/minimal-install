fe8b9d6bdd388a3456d3b2099cf9f4b6349f6880
Guilherme Braga <guilherme.bl90@gmail.com>
2024-06-15 09:56:13 -0300

    fix picom.conf

--- config_xfce.sh
+++ config_xfce.sh
@@ -24,15 +24,6 @@ cp -R -f utils/configs/xfce4/* "$xfce4_dir" && \
 
 mkdir "/home/$USER/.config/picom" && \
 
-cp update-xfce-bigsur/picom/picom.conf "/home/$USER/.config/picom" && \
-
-# Arquivo picom.conf
-picom_conf="/home/$USER/.config/picom/picom.conf" && \
-
-# Comentar linhas específicas
-sed -i 's/^inactive-opacity = 0.8/#inactive-opacity = 0.8/' "$picom_conf" && \
-sed -i 's/^frame-opacity = 0.7/#frame-opacity = 0.7/' "$picom_conf" && \
-sed -i 's/popup_menu = { opacity = 0.8; }/#popup_menu = { opacity = 0.8; }/' "$picom_conf" && \
-sed -i 's/dropdown_menu = { opacity = 0.8; }/#dropdown_menu = { opacity = 0.8; }/' "$picom_conf" && \
+cp utils/configs/picom/picom.conf "/home/$USER/.config/picom" && \
 
 echo 'Configuração do ambiente xfce concluída!'
