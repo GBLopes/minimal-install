b20e17919e3a177acf4240d3ad850571cfb5ddec
Guilherme Braga <guilherme.bl90@gmail.com>
2024-06-19 22:07:13 -0300

    Retornando libreoffice ao padrão

--- install.sh
+++ install.sh
@@ -82,11 +82,10 @@ cp utils/qalculate/qalculate.svg /usr/share/icons && \
 cp utils/qalculate/qalculate-gtk.desktop /usr/share/applications && \
 
 #LibreOffice:
-#curl -LO https://download.documentfoundation.org/libreoffice/stable/24.2.4/deb/x86_64/LibreOffice_24.2.4_Linux_x86-64_deb.tar.gz && \
-#tar -xf LibreOffice_24.2.4_Linux_x86-64_deb.tar.gz && \
-#dpkg -i LibreOffice_24.2.4.2_Linux_x86-64_deb/DEBS/*.deb && \
-#rm -rf LibreOffice_24.2.4_Linux_x86-64_deb.tar.gz LibreOffice_24.2.4.2_Linux_x86-64_deb && \
-apt install -y libreoffice && \
+curl -LO https://download.documentfoundation.org/libreoffice/stable/24.2.4/deb/x86_64/LibreOffice_24.2.4_Linux_x86-64_deb.tar.gz && \
+tar -xf LibreOffice_24.2.4_Linux_x86-64_deb.tar.gz && \
+dpkg -i LibreOffice_24.2.4.2_Linux_x86-64_deb/DEBS/*.deb && \
+rm -rf LibreOffice_24.2.4_Linux_x86-64_deb.tar.gz LibreOffice_24.2.4.2_Linux_x86-64_deb && \
 curl -LO https://download.documentfoundation.org/libreoffice/stable/24.2.4/deb/x86_64/LibreOffice_24.2.4_Linux_x86-64_deb_langpack_pt-BR.tar.gz && \
 tar -xf LibreOffice_24.2.4_Linux_x86-64_deb_langpack_pt-BR.tar.gz && \
 dpkg -i LibreOffice_24.2.4.2_Linux_x86-64_deb_langpack_pt-BR/DEBS/*.deb && \
