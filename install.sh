1325f045a1f59622a81c1b65f9eff49ad77f25fc
GBLopes <guilherme.bl90@gmail.com>
2024-06-21 17:43:47 -0300

    comment grub theme

--- install.sh
+++ install.sh
@@ -274,16 +274,16 @@ fi && \
 cp utils/configs/alacritty/alacritty.yml $HOME/.config/alacritty && \
 
 #GRUB Catpuccin theme
-if [ ! -d "/usr/share/grub/themes" ]; then
-    echo "Criando pasta /usr/share/grub/themes..."
-    mkdir -p "/usr/share/grub/themes"
-fi && \
-git clone https://github.com/catppuccin/grub.git && \
-cp -r grub/src/* /usr/share/grub/themes/ && \
-echo '
-GRUB_THEME="/usr/share/grub/themes/catppuccin-mocha-grub-theme/theme.txt"
-' | tee -a '/etc/default/grub' && \
-update-grub && \
+# if [ ! -d "/usr/share/grub/themes" ]; then
+#     echo "Criando pasta /usr/share/grub/themes..."
+#     mkdir -p "/usr/share/grub/themes"
+# fi && \
+# git clone https://github.com/catppuccin/grub.git && \
+# cp -r grub/src/* /usr/share/grub/themes/ && \
+# echo '
+# GRUB_THEME="/usr/share/grub/themes/catppuccin-mocha-grub-theme/theme.txt"
+# ' | tee -a '/etc/default/grub' && \
+# update-grub && \
 
 #Gedit Catpuccin theme
 git clone https://github.com/catppuccin/gedit.git && \
