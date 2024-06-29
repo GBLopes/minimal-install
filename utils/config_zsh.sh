03f068a47cc35af2b95d0abe1e12c5dc8667a868
Guilherme Braga <guilherme.bl90@gmail.com>
2024-06-29 16:28:53 -0300

    fix typo zshrc path

--- utils/config_zsh.sh
+++ utils/config_zsh.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cp "utils/.zshrc" "$HOME/.zshrc" && \
+cp "assets/.zshrc" "$HOME/.zshrc" && \
 chsh -s /bin/zsh && \
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
