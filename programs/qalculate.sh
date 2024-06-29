944902cca411316016e8eb393f35d849c4540f37
Guilherme Braga <guilherme.bl90@gmail.com>
2024-06-29 16:48:14 -0300

    fix typo

--- programs/qalculate.sh
+++ programs/qalculate.sh
@@ -3,5 +3,5 @@
 curl -LO 'https://github.com/Qalculate/libqalculate/releases/download/v5.1.1/qalculate-5.1.1-x86_64.tar.xz' && \
 tar -xvf qalculate-5.1.1-x86_64.tar.xz && rm qalculate-5.1.1-x86_64.tar.xz && \
 mv qalculate-5.1.1 /opt && \
-cp /assets/qalculate/qalculate.svg /usr/share/icons && \
-cp /assets/qalculate/qalculate-gtk.desktop /usr/share/applications
+cp assets/qalculate/qalculate.svg /usr/share/icons && \
+cp assets/qalculate/qalculate-gtk.desktop /usr/share/applications
